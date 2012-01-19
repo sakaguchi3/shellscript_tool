42ec10b4ccc770b7a385dcc3d0d6acad2eca3f87
uqw <uqw@outlook.jp>
2012-01-19 19:29:14 +0900

    rsync sshkey option

--- sh_tool/memo_rsync.sh
+++ sh_tool/memo_rsync.sh
@@ -22,24 +22,28 @@
 # function --------------------------------------------------
 
 func_rsync_make_dir() {
-  dir_nest="/tmp/x/y/z"
-  ssh_user="usr"
-  ssh_host="localhost"
+  lcoal dir_nest="/tmp/x/y/z"
+  lcoal ssh_user="usr"
+  lcoal ssh_host="localhost"
+  local ssh_key="~/.ssh/ssh_private"
 
   # copy to remote on '/tmp/x/y/z'
   rsync -a \
+    -e "ssh -i ${ssh_key}" \
     --rsync-path="mkdir -p ${dir_nest} && rsync ${dir_nest}" \
     "${ssh_user}@${ssh_host}:/${dir_nest}"
 
 }
 
 func_rsync_relative() {
-  dir_nest="/tmp/x/y/z"
-  ssh_user="usr"
-  ssh_host="localhost"
+  lcoal dir_nest="/tmp/x/y/z"
+  lcoal ssh_user="usr"
+  lcoal ssh_host="localhost"
+  local ssh_key="~/.ssh/ssh_private"
 
   # copy to remote on '/pre_exsisting/tmp/x/y/z'
   rsync -a \
+    -e "ssh -i ${ssh_key}" \
     --relative "${dir_nest}" "${ssh_user}@${ssh_host}":/pre_existing/
 
 }
